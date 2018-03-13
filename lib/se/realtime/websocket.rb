8f4d85873d4cfd80934594410da8602a213344ba
izwick-schachter <isaiah@developingtechnician.com>
2018-03-13 17:52:30 -0400

    Added logging because death is sad

--- lib/se/realtime/websocket.rb
+++ lib/se/realtime/websocket.rb
@@ -12,20 +12,28 @@ module SE
       attr_reader :url, :thread, :driver
       attr_accessor :handler
 
+      # If you're looking back it this, look at super_logger (ws_super_logger, panic switch logging) and logger (realtime, messages (and some warnings))
+
       def initialize(url, cookies, &handler)
+        @super_logger = Logger.new('ws_super_logger.log')
+
         @uri = URI.parse(url)
         @url = "ws#{@uri.scheme.split("")[4]}://#{@uri.host}"
         @driver = WebSocket::Driver.client(self)
         if @uri.scheme.split("")[4] == 's'
           @socket = TCPSocket.new(@uri.host, 443)
+          @super_logger.info "Opened TCP socket for (port 443) #{@uri} (#{@socket})"
           @socket = OpenSSL::SSL::SSLSocket.new(@socket)
           @socket.connect
+          @super_logger.info "Upgrade TCP socket to SSL socket socket for #{@uri} (#{@socket})"
         else
           @socket = TCPSocket.new(@uri.host, 80)
+          @super_logger.info "Opened TCP socket for (port 80) #{@uri} (#{@socket})"
         end
         @handler = handler
         @logger = Logger.new "realtime.log"
         @restart = true
+        @super_logger.info "Set @restart to #{@restart}"
 
         @driver.add_extension PermessageDeflate
         @driver.set_header "Cookies", cookies if cookies
@@ -35,6 +43,7 @@ module SE
 
         @driver.on :open, ->(_e) do
           send "155-questions-active"
+          @super_logger.info "Socket open. Subscribed to 155-questions-active"
           @logger.info "WebSocket is open!"
         end
 
@@ -50,8 +59,10 @@ module SE
 
         @driver.on :close, ->(_e) do
           @logger.info "Realtime WebSocket is closing."
+          @super_logger.info "Socket was closed. @restart == #{@restart}"
           if @restart
             @logger.info "Attempting to reopen websocket..."
+            @super_logger.info "Attempting to reopen socket"
             @driver.start
           end
         end
@@ -63,6 +74,7 @@ module SE
         @thread = Thread.new do
           trap("SIGINT") do
             @restart = false
+            @super_logger.info "Got SIGINT. Dying."
             close
             Thread.exit
           end
@@ -70,9 +82,11 @@ module SE
             begin
               @driver.parse(@socket.is_a?(TCPSocket) ? @socket.recv(1) : @socket.sysread(1))
             rescue IOError, SystemCallError => e
+              @super_logger.warn "Got some kind of interrupt in the thread. Panic."
               @logger.warn "Recieved #{e} closing TCP socket. You shouldn't be worried :)"
             end
           end
+          @super_logger.warn "Left TCPSocket.recv loop. If you're reading this, panic."
         end
 
         at_exit { @thread.join }
@@ -89,6 +103,7 @@ module SE
       end
 
       def close
+        @super_logger.warn "Was told to close. Was sad."
         @driver.close
         @socket.is_a?(TCPSocket) ? @socket.shutdown : @socket.sysclose
       rescue IOError, Errno::ENOTCONN => e
